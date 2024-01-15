5487a601d0335ff9fc02d878c2ac0785fb72ba95
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-01-15 09:49:25 -0800

    Create Add.cs

---
+++ Add.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+      Console.WriteLine(sum); // Print the sum of x + y
+    }
+  }
+}
