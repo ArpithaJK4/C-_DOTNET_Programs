29eb7a1bf45944ab9d16542ecf9cae18d1d1b718
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-01-15 09:26:06 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+
+Result Size: 649 x 484
+using System;
+​
+namespace MyApplication
+{
+  // Interface
+  interface IAnimal 
+  {
+    void animalSound(); // interface method (does not have a body)
+  }
+​
+  // Pig "implements" the IAnimal interface
+  class Pig : IAnimal 
+  {
+    public void animalSound() 
+    {
+      // The body of animalSound() is provided here
+      Console.WriteLine("The pig says: wee wee");
+    }
+  }
+​
+  class Program 
+  {
+    static void Main(string[] args) 
+    {
+      Pig myPig = new Pig();  // Create a Pig object
+      myPig.animalSound();
+    }
+  }
+}
+The pig says: wee wee
