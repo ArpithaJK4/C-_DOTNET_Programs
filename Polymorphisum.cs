2d7db113929e54cb8cad4ee90ab51ddf7fadd21c
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-01-18 06:15:49 -0800

    Create Polymorphisum.cs

---
+++ Polymorphisum.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace MyApplication
+{
+  class Animal  // Base class (parent) 
+  {
+    public void animalSound()
+    {
+      Console.WriteLine("The animal makes a sound");
+    }
+  }
+
+  class Pig : Animal  // Derived class (child) 
+  {
+    public void animalSound()
+    {
+      Console.WriteLine("The pig says: wee wee");
+    }
+  }
+
+  class Dog : Animal  // Derived class (child) 
+  {
+    public void animalSound()
+    {
+      Console.WriteLine("The dog says: bow wow");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Animal myAnimal = new Animal();  // Create a Animal object
+      Animal myPig = new Pig();  // Create a Pig object
+      Animal myDog = new Dog();  // Create a Dog object
+      
+      myAnimal.animalSound();
+      myPig.animalSound();
+      myDog.animalSound();
+    }
+  }
+}
