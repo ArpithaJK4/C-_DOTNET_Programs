f8a59f8a817f9ca913ad6de2cf9362e875235a13
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-01-25 10:35:33 -0800

    Create practice.cs

---
+++ practice.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 7;
+        
+        Console.WriteLine(x == 7); 
+        
+        Console.WriteLine(x!=8);
+        Console.WriteLine(x > 10);
+        Console.WriteLine(x < 2);
+        Console.WriteLine(x >= 7);
+        Console.WriteLine(x <= 7);
+    }
+}
